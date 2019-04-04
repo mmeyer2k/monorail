d87ca589d368b52b44e8443099cee9adeb9d0326
mmeyer2k <m.meyer2k@gmail.com>
2019-04-04 12:52:17 -0500

    Update Queue.php

--- src/Queue.php
+++ src/Queue.php
@@ -4,5 +4,19 @@ namespace mmeyer2k\Monorail;
 
 class Queue
 {
-
+    public static function push(\Predis\Client $redis, \Closure $closure, string $tube = 'default')
+    {
+        $id = md5(mt_rand());
+    
+        $serializer = new \SuperClosure\Serializer();
+    
+        $serialized = $serializer ->serialize($closure);
+    
+        $job = json_encode([
+            'id' => $id,
+            'closure' => $serialized,
+        ]);
+        
+        $redis->lpush("monorail:$tube:active", $job);
+    }
 }

c6f1801342672a076db4bf0201821a9b52e2cac1
vagrant <vagrant@homestead>
2019-04-04 18:25:25 +0000

    local commit

--- src/Queue.php
+++ src/Queue.php
@@ -7,16 +7,16 @@ class Queue
     public static function push(\Predis\Client $redis, \Closure $closure, string $tube = 'default')
     {
         $id = md5(mt_rand());
-    
+
         $serializer = new \SuperClosure\Serializer();
-    
-        $serialized = $serializer ->serialize($closure);
-    
+
+        $serialized = $serializer->serialize($closure);
+
         $job = json_encode([
             'id' => $id,
             'closure' => $serialized,
         ]);
-        
+
         $redis->lpush("monorail:$tube:active", $job);
     }
 }
--- src/QueueWorker.php
+++ src/QueueWorker.php
@@ -4,28 +4,28 @@ namespace mmeyer2k\Monorail;
 
 class QueueWorker
 {
-    public static function consoleLog(string $msg) 
+    public static function consoleLog(string $msg)
     {
     }
-  
+
     public static function popJob(\Predis\Client $redis, string $tube): bool
     {
         $jobRaw = $redis->lindex("monorail:$tube:active", -1);
 
         $job = json_decode($jobRaw);
-    
+
         if (!$job) {
             return false;
         }
-    
+
         $sem = sem_get(666);
-    
+
         sem_acquire($sem);
-    
+
         echo "processing... [$job->id]\n";
-    
+
         $serializer = new \SuperClosure\Serializer();
-        
+
         $closure = $serializer->unserialize($job->closure);
 
         // Increment job failed counter here and save back to redis
@@ -39,7 +39,7 @@ class QueueWorker
         } catch (\Exception $e) {
             $exmsg = $e->getMessage();
         }
-        
+
         $redis->multi();
 
         if ($exmsg) {
@@ -51,18 +51,18 @@ class QueueWorker
 
             echo "failed...     [$job->id][$fails][$exmsg]\n";
         } else {
-            
+
             $redis->rpop("queue:active");
 
             $redis->del("queue:failed:$job->id");
 
             echo "processed...  [$job->id]\n";
         }
-            
+
         $redis->exec();
-    
+
         sem_release($sem);
-    
+
         return true;
     }
 }

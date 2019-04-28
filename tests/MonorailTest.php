4570a8aa49805aa90f020d572562239ed6a7e8ca
mmeyer2k <m.meyer2k@gmail.com>
2019-04-28 06:19:33 -0500

    no message

---
+++ tests/MonorailTest.php
@@ -0,0 +1,70 @@
+<?php
+
+class MonorailTest extends \PHPUnit\Framework\TestCase
+{
+    function setUp()
+    {
+        foreach (range(1, 9) as $p) {
+            foreach (range(0, 9) as $i) {
+                echo "pushing priority:$p #$i\n";
+                (new \mmeyer2k\Monorail\Task)
+                    ->priority($p)
+                    ->push(function () {
+                        $redis = new \Predis\Client;
+                        $redis->incr('test_count');
+                    });
+            }
+        }
+
+        foreach (range(1, 9) as $p) {
+            foreach (range(0, 9) as $i) {
+                echo "pushing priority:$p #$i\n";
+                (new \mmeyer2k\Monorail\Task)
+                    ->priority($p)
+                    ->push(function () {
+                        $redis = new \Predis\Client;
+                        $redis->incr('test_count_failed');
+                        throw new \Exception("WoMp wOmP");
+                    });
+            }
+        }
+
+        foreach (range(1, 9) as $p) {
+            foreach (range(0, 9) as $i) {
+                echo "pushing priority:$p #$i [delayed]\n";
+                (new \mmeyer2k\Monorail\Task)
+                    ->priority($p)
+                    ->delay(1)
+                    ->push(function () {
+                        $redis = new \Predis\Client;
+                        $redis->incr('test_count_delayed');
+                    });
+            }
+        }
+
+        foreach (range(1, 9) as $p) {
+            foreach (range(0, 9) as $i) {
+                echo "pushing priority:$p #$i [delayed][tube2]\n";
+                (new \mmeyer2k\Monorail\Task)
+                    ->priority($p)
+                    ->tube('tube2')
+                    ->delay(1)
+                    ->push(function () {
+                        $redis = new \Predis\Client;
+                        $redis->incr('test_count_delayed_tube2');
+                    });
+            }
+        }
+
+        sleep(2);
+
+        require __DIR__ . '/../work.php';
+    }
+
+    function testCounters()
+    {
+        $redis = new \Predis\Client;
+
+        $this->assertEquals(90, (int)$redis->get("test_count"));
+    }
+}
--- work.php
+++ work.php
@@ -6,8 +6,6 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require __DIR__ . '/../../autoload.php';
 }
 
-$single = in_array('--single', $argv);
-
 $redis = new Predis\Client;
 
 foreach (range(1, 9) as $priority) {

ca63ce0a60d7010a51790e3ea7e99e43850bd925
mmeyer2k <m.meyer2k@gmail.com>
2019-04-28 02:26:05 -0500

    no message

--- tests/1.push.php
+++ tests/1.push.php
@@ -3,19 +3,19 @@
 require __DIR__ . "/../vendor/autoload.php";
 
 foreach (range(0, 9) as $p) {
-    foreach (range(0, 999) as $i) {
+    foreach (range(0, 99) as $i) {
         echo "pushing priority:$p #$i\n";
-
-        // Queue a job with the basic syntax default tube no delay
         (new \mmeyer2k\Monorail\Task)
             ->priority($p)
             ->push(function () {
                 \Redis::incr('test_count');
             });
+    }
+}
 
-
-        echo "pushing priority:$p #$i\n [delayed]";
-        // Queue a job with the basic syntax default tube no delay
+foreach (range(0, 9) as $p) {
+    foreach (range(0, 99) as $i) {
+        echo "pushing priority:$p #$i [delayed]\n";
         (new \mmeyer2k\Monorail\Task)
             ->priority($p)
             ->delay(2)
